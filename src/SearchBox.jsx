09cde9fb739b8c81ff80c995c013346e9947ed66
Muhammad Hassan <mha9441100@gmail.com>
2024-03-07 12:11:36 +0500

    Update changes

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -4,19 +4,21 @@ const API_KEY = import.meta.env.VITE_REACT_APP_API_KEY;
 const API_URL = import.meta.env.VITE_REACT_APP_API_URL;
 
 
+
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import "./SearchBox.css"
 import { useState } from 'react';
 export default function SearchBox({ updateInfo }) {
-    
+    console.log(API_KEY);
+    console.log(API_URL);
     let [city, setCity] = useState("");
     let [error, setError] = useState(false);
     let getWeatherInfo = async () => {
        try {
         let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
         let jsonResponse = await response.json();
-  
+        console.log(jsonResponse);
         let result = {
             city: city,
             temp: jsonResponse.main.temp,
@@ -26,7 +28,7 @@ export default function SearchBox({ updateInfo }) {
             feelsLike: jsonResponse.main.feels_like,
             desc: jsonResponse.weather[0].description,
         }
-     
+        console.log(result);
         return result;
        } catch (err) {
         throw err;
